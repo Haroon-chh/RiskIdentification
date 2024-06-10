c58733008c6107300f1f9e8e5bea1ac1f86de583
haroon-chh <haroon4501588@gmail.com>
2024-06-11 01:44:26 +0500

    for vercel

--- Risk/Risk/settings.py
+++ Risk/Risk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$ut16-^1f6zhq45-lm$$27)k1bmm_f7p^y#*@bx0yzy+nw+4#r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -132,3 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #third setting for corsheader
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
